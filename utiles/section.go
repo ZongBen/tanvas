781442a405975c6d7cabadc60faff85717cd01b9
TFSAdmin <TFSAdmin>
2024-07-16 10:47:51 +0800

    update

--- utiles/section.go
+++ utiles/section.go
@@ -6,12 +6,6 @@ type section struct {
 	char   [][]*rune
 }
 
-func (s *section) CreateTextArea(autoWrap bool) textarea {
-	area := CreateTextArea(s.width, s.height, autoWrap)
-	area.char = s.char
-	return area
-}
-
 func (s *section) SetChar(row, col int, char rune) {
 	*s.char[row][col] = char
 }
