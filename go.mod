9ea157fe3357d1caaeb61b75f0611fd0fe3a9d6a
TFSAdmin <TFSAdmin>
2024-07-29 17:59:35 +0800

    add feature

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/ZongBen/tanvas
 
 go 1.22.3
+
+require (
+	github.com/mattn/go-runewidth v0.0.9 // indirect
+	github.com/nsf/termbox-go v1.1.1 // indirect
+)
---
+++ tanminal/tanminal.go
@@ -0,0 +1,80 @@
+package tanminal
+
+import (
+	"strings"
+	"sync"
+
+	"github.com/ZongBen/tanvas"
+	"github.com/nsf/termbox-go"
+)
+
+func init() {
+	if !termbox.IsInit {
+		termbox.Init()
+	}
+}
+
+type Tanminal struct {
+	offset_x              int
+	offset_y              int
+	horizontalAlignCenter bool
+	verticalAlignCenter   bool
+}
+
+func (t *Tanminal) Flush(c *tanvas.Canvas) {
+	offsetX, offsetY := t.getOffset()
+	Clear()
+	wg := new(sync.WaitGroup)
+	lines := strings.Split(c.Project(), "\n")
+	for y, line := range lines {
+		wg.Add(1)
+		go func(y int, line string) {
+			for x, char := range line {
+				termbox.SetChar(x+offsetX, y+offsetY, char)
+			}
+			wg.Done()
+		}(y, line)
+	}
+	wg.Wait()
+	termbox.Flush()
+}
+
+func Clear() {
+	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+}
+
+func Close() {
+	termbox.Close()
+}
+
+func (t *Tanminal) AlignCenter(offsetX, offsetY int) {
+	t.AlignCenterX(offsetX)
+	t.AlignCenterY(offsetY)
+}
+
+func (t *Tanminal) AlignCenterX(offsetX int) {
+	t.horizontalAlignCenter = true
+	t.offset_x = offsetX
+}
+
+func (t *Tanminal) AlignCenterY(offsetY int) {
+	t.verticalAlignCenter = true
+	t.offset_y = offsetY
+}
+
+func getTerminalCenter() (int, int) {
+	width, height := termbox.Size()
+	return width / 2, height / 2
+}
+
+func (t *Tanminal) getOffset() (int, int) {
+	width, height := getTerminalCenter()
+	offsetX, offsetY := 0, 0
+	if t.horizontalAlignCenter {
+		offsetX = width + t.offset_x
+	}
+	if t.verticalAlignCenter {
+		offsetY = height + t.offset_y
+	}
+	return offsetX, offsetY
+}
